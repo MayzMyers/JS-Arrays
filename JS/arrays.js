27bab93e9cb5c0a7bfd26060186845e5d0073711
MayzMyers <35607480+MayzMyers@users.noreply.github.com>
2023-11-05 22:13:00 +0300

    Revert "Решили первую задачу"
    This reverts commit a79c4104231851aafeb179326e95c2c6294d50fb.

--- JS/arrays.js
+++ JS/arrays.js
@@ -1,8 +1,1 @@
-function task1() {
-  let arr = [4, 16, 36, 25];
-  let summ = arr.reduce(function (sum, elem) {
-    if (elem % 2 == 0) sum += Math.sqrt(elem);
-    return sum;
-  }, 0);
-  alert(summ);
-}
+s
