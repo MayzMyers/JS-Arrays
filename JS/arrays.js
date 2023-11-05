5fba6149d9f0ded20bb818049f87eaa53e4f9d07
MayzMyers <35607480+MayzMyers@users.noreply.github.com>
2023-11-05 22:26:57 +0300

    Изменение JS файла

--- JS/arrays.js
+++ JS/arrays.js
@@ -6,3 +6,39 @@ function task1() {
   }, 0);
   alert(summ);
 }
+
+function task2() {
+  alert("task2");
+}
+
+function task3() {
+  alert("task3");
+}
+
+function task4() {
+  alert("task4");
+}
+
+function task5() {
+  alert("task5");
+}
+
+function task6() {
+  alert("task6");
+}
+
+function task7() {
+  alert("task7");
+}
+
+function task8() {
+  alert("task8");
+}
+
+function task9() {
+  alert("task9");
+}
+
+function task10() {
+  alert("task10");
+}
